83fa948e5c7660900aa4320024c31b00ac6c771d
Bhanuteja917 <bhanutejach9@gmail.com>
2023-05-21 20:10:21 +0530

    Added Animations

--- src/views/QuizzesView.vue
+++ src/views/QuizzesView.vue
@@ -2,6 +2,7 @@
   import q from "../data/quizzes.json"
   import {ref, watch} from "vue"
   import Card from "../components/Card.vue"
+  import gsap from "gsap"
 
   const quizzes = ref(q)
   const search = ref("")
@@ -9,6 +10,26 @@
   watch(search, () => {
     quizzes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
   })
+
+  const beforeEnter = (el) => {
+    gsap.from(el, {
+      y: -100,
+      opacity: 0,
+    })
+  }
+
+  const enter = (el) => {
+    gsap.to(el, {
+      y: 0,
+      opacity: 1,
+      duration: 0.3,
+      stagger: 0.3
+    })
+  }
+
+  // const afterEnter = () => {
+
+  // }
 </script>
 
 <template>
@@ -18,7 +39,19 @@
       <input v-model.trim="search" type="text" placeholder="Search...">
     </header>
     <div class="options-container">
-      <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz"/>
+      <TransitionGroup 
+        appear
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @afterEnter="afterEnter"
+      >
+        <Card 
+          v-for="(quiz, index) in quizzes" 
+          :key="quiz.id" 
+          :quiz="quiz"
+          :data-index="index"
+        />
+      </TransitionGroup>
     </div>
   </div>
 </template>
